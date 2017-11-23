f5f80b40e260dd79b4fa541341672672235419b2
hjkim86 <hjkim86@sptek.co.kr>
2017-11-23 18:03:19 +0900

    remove configuration.h

--- SDK_Simple/renesas/src/SimpleAPI/middleware/Configuration.h
+++ SDK_Simple/renesas/src/SimpleAPI/middleware/Configuration.h
@@ -10,16 +10,16 @@
 #ifndef _CONFIGURATION_H_
 #define _CONFIGURATION_H_
 
-#define MQTT_HOST                           "218.53.242.111"
-#define MQTT_SECURE_HOST                    "ssl://218.53.242.111"
+#define MQTT_HOST                           "(TBD)"
+#define MQTT_SECURE_HOST                    
 #define MQTT_PORT                           1883
 #define MQTT_SECURE_PORT                    8883
 #define MQTT_KEEP_ALIVE                     120
 #define MQTT_ENABLE_SERVER_CERT_AUTH        0
 
-#define LOGIN_NAME                          "6974d730bd1411e7a0db"
+#define LOGIN_NAME                          "(TBD)"
 
-#define SIMPLE_SERVICE_NAME                 "middleware"
-#define SIMPLE_DEVICE_NAME                  "bbb02"
+#define SIMPLE_SERVICE_NAME                 "(TBD)"
+#define SIMPLE_DEVICE_NAME                  "(TBD)"
 
 #endif // _CONFIGURATION_H_
