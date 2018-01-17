1146dddbf5ce74f60a10699d0b07526f5b546c1c
takeej <takeej@sptek.co.kr>
2018-01-17 15:27:09 +0900

    Simple SDK for JavaME

---
+++ SDK_Simple/JavaME/SampleForSimpleSDK/src/sample/Config.java
@@ -0,0 +1,24 @@
+package sample;
+
+/**
+ * Config.java
+ * <p>
+ * Copyright (C) 2017. SK Telecom, All Rights Reserved.
+ * Written 2017, by SK Telecom
+ */
+public class Config {
+
+    public static final String SIMPLE_HOST = "tcp://218.53.242.111";
+    public static final String SIMPLE_SECURE_HOST = "ssl://218.53.242.111";
+    
+    public static final int SIMPLE_PORT = 1883;
+    public static final int SIMPLE_SECURE_PORT = 8883;
+
+    public static final int SIMPLE_KEEP_ALIVE = 300;
+    
+    public static final String DEVICE_TOKEN = "748e9eb0f6a511e78ba2";
+
+    public static final String SIMPLE_SERVICE_NAME = "myservice";
+    public static final String SIMPLE_DEVICE_NAME = "javame";
+}
+
